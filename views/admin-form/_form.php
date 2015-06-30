17948aaa6692eaf11e366d8d106da2a3c008a30b
Semenov <semenov@skeeks.com>
2015-06-30 15:50:16 +0400

    Исправлена ошибка сортировки полей в форме

--- views/admin-form/_form.php
+++ views/admin-form/_form.php
@@ -36,23 +36,40 @@ use common\models\User;
                     ]
                 ],
 
+                'dataProviderCallback' => function($dataProvider)
+                {
+                    /**
+                     * @var \yii\data\BaseDataProvider $dataProvider
+                     */
+                    $dataProvider->getPagination()->defaultPageSize   = 5000;
+                },
+
                 'controllerRoute'   => 'form2/admin-form-property',
                 'gridViewOptions'   => [
                     'sortable' => true,
                     'columns' => [
-                        'name',
-
+                        [
+                            'attribute'     => 'name',
+                            'enableSorting' => false
+                        ],
 
                         [
                             'class'         => \skeeks\cms\grid\BooleanColumn::className(),
                             'attribute'     => 'active',
                             'falseValue'    => \skeeks\cms\components\Cms::BOOL_N,
-                            'trueValue'     => \skeeks\cms\components\Cms::BOOL_Y
+                            'trueValue'     => \skeeks\cms\components\Cms::BOOL_Y,
+                            'enableSorting' => false
                         ],
 
+                        [
+                            'attribute'     => 'code',
+                            'enableSorting' => false
+                        ],
 
-                        'code',
-                        'priority',
+                        [
+                            'attribute'     => 'priority',
+                            'enableSorting' => false
+                        ],
                     ],
                 ],
             ]); ?>
