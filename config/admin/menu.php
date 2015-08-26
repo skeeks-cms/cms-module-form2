01670aa348debbcdd26ae888d3627678af7e0ee7
Semenov <semenov@skeeks.com>
2015-08-26 11:19:07 +0300

    Изменения по административной части

--- config/admin/menu.php
+++ config/admin/menu.php
@@ -7,25 +7,30 @@
  */
 return [
 
-    'form2' =>
+    'other' =>
     [
-        'label'     => 'Конструктор форм',
-        "img"       => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/forms.png'],
-
         'items' =>
         [
             [
-                "label" => "Формы",
-                "url"   => ["form2/admin-form"],
+                "label"     => "Конструктор форм",
                 "img"       => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/forms.png'],
-            ],
 
-            [
-                "label" => "Сообщения с форм",
-                "url"   => ["form2/admin-form-send"],
-                "img"       => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/form-submits.png'],
-            ],
+                'items' =>
+                [
+                    [
+                        "label" => "Формы",
+                        "url"   => ["form2/admin-form"],
+                        "img"       => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/forms.png'],
+                    ],
 
+                    [
+                        "label" => "Сообщения с форм",
+                        "url"   => ["form2/admin-form-send"],
+                        "img"       => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/form-submits.png'],
+                    ],
+
+                ]
+            ],
         ]
     ]
 ];
