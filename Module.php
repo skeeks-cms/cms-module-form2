7341b52e8819524815df4ecf306581d76d151f26
Semenov Alexander <semenov@skeeks.com>
2016-04-19 18:55:05 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -10,7 +10,7 @@ namespace skeeks\modules\cms\form2;
  * Class Module
  * @package skeeks\modules\cms\form2
  */
-class Module extends \yii\web\Module
+class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'skeeks\modules\cms\form2\controllers';
 }
