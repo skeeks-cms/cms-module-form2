7b58f1acd7f15d38f80b4f85581da9fae7dabd04
semenov <semenov@skeeks.com>
2019-01-25 19:21:00 +0300

    fixed

---
+++ src/config/common.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 07.03.2015
+ */
+return [
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'skeeks/form2/app' => [
+                    'class'    => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@skeeks/modules/cms/form2/messages',
+                    'fileMap'  => [
+                        'skeeks/form2/app' => 'app.php',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
---
+++ src/config/web.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 15.06.2015
+ */
+return [
+    'modules' => [
+        'form2' => [
+            'class' => '\skeeks\modules\cms\form2\Module',
+        ],
+    ],
+];
