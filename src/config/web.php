fcb6913e83e9863471f0d01815bc035e2b82a5ea
Semenov <semenov@skeeks.com>
2019-11-18 13:52:29 +0300

    dev

--- src/config/web.php
+++ src/config/web.php
@@ -6,6 +6,38 @@
  * @date 15.06.2015
  */
 return [
+    'components' => [
+        'backendAdmin' => [
+            'menu' => [
+                'data' => [
+                    'other' => [
+                        'items' => [
+                            [
+                                "name"  => ['skeeks/form2/app', 'Form designer'],
+                                "image" => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/forms.png'],
+
+                                'items' => [
+                                    [
+                                        "name"  => ['skeeks/form2/app', 'Forms'],
+                                        "url"   => ["form2/admin-form"],
+                                        "image" => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/forms.png'],
+                                    ],
+
+                                    [
+                                        "name"  => ['skeeks/form2/app', 'Messages'],
+                                        "url"   => ["form2/admin-form-send"],
+                                        "image" => ['\skeeks\modules\cms\form2\assets\FormAsset', 'icons/form-submits.png'],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+
+                ],
+            ],
+        ],
+    ],
+
     'modules' => [
         'form2' => [
             'class' => '\skeeks\modules\cms\form2\Module',
