f53aa46f649be50417776ec4940a32096036b296
Semenov <semenov@skeeks.com>
2020-02-21 17:34:15 +0300

    dev

--- src/widgets/ActiveFormConstructForm.php
+++ src/widgets/ActiveFormConstructForm.php
@@ -5,17 +5,14 @@
  * @copyright 2010 SkeekS (�����)
  * @date 18.03.2015
  */
+
 namespace skeeks\modules\cms\form2\widgets;
 
-use skeeks\cms\modules\admin\widgets\ActiveForm;
 use skeeks\cms\modules\admin\traits\ActiveFormTrait;
 use skeeks\cms\modules\admin\traits\AdminActiveFormTrait;
 use skeeks\cms\traits\ActiveFormAjaxSubmitTrait;
 use skeeks\modules\cms\form\models\Form;
 use skeeks\modules\cms\form2\models\Form2Form;
-use skeeks\widget\chosen\Chosen;
-use yii\base\Model;
-use yii\helpers\ArrayHelper;
 
 /**
  * Class ActiveFormRelatedProperties
@@ -35,12 +32,12 @@ class ActiveFormConstructForm extends \skeeks\cms\base\widgets\ActiveForm
 
     public function __construct($config = [])
     {
-        $this->validationUrl                = \skeeks\cms\helpers\UrlHelper::construct('form2/backend/validate')->toString();
-        $this->action                       = \skeeks\cms\helpers\UrlHelper::construct('form2/backend/submit')->toString();
+        $this->validationUrl = \skeeks\cms\helpers\UrlHelper::construct('form2/backend/validate')->toString();
+        $this->action = \skeeks\cms\helpers\UrlHelper::construct('form2/backend/submit')->toString();
 
-        $this->enableAjaxValidation         = true;
-        $this->validateOnChange         = false;
-        $this->validateOnBlur         = false;
+        $this->enableAjaxValidation = true;
+        $this->validateOnChange = false;
+        $this->validateOnBlur = false;
 
         parent::__construct($config);
     }
@@ -49,7 +46,7 @@ class ActiveFormConstructForm extends \skeeks\cms\base\widgets\ActiveForm
     {
         parent::init();
 
-        echo \yii\helpers\Html::hiddenInput("sx-model-value",   $this->modelForm->id);
-        echo \yii\helpers\Html::hiddenInput("sx-model",         $this->modelForm->className());
+        echo \yii\helpers\Html::hiddenInput("sx-model-value", $this->modelForm->id);
+        echo \yii\helpers\Html::hiddenInput("sx-model", $this->modelForm->className());
     }
 }
