e133b7c1499569560fb8e3ceedbafbd21ea969e9
Semenov <semenov@skeeks.com>
2025-11-19 14:14:12 +0300

    dev

--- src/cmsWidgets/form2/views/fancybox.php
+++ src/cmsWidgets/form2/views/fancybox.php
@@ -83,7 +83,7 @@ JS
 <? endif; ?>
 
 <?php if ($widget->modelForm->is_add_legal_checkbox) : ?>
-<div class="form-group field-relatedpropertiesmodel-phone required has-error">
+<div class="form-group field-rules required has-error">
     <input type="checkbox" required id="sx-rules">
     <label for="sx-rules"><?php echo $widget->modelForm->legalCheckboxText; ?></label>
 </div>
